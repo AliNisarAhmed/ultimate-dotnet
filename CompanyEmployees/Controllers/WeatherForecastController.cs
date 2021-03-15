cc186c97e4def387a2f2d62469154af0865bbab7
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-03-15 16:23:19 -0600

    Chap 2

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoggerService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private ILoggerManager _logger;
+
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            this._logger = logger;
+        }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Info");
+            _logger.LogDebug("Debug");
+            _logger.LogWarn("Warn");
+            _logger.LogError("Error");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
