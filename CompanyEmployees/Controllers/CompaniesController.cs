e330d05a2a9537d0d20da1b274d3b16f9b1b525b
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-06-10 20:12:53 -0600

    supporting root document

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -29,7 +29,7 @@ namespace CompanyEmployees.Controllers
 			_mapper = mapper;
 		}
 
-		[HttpGet]
+		[HttpGet(Name = "GetCompanies")]
 		public async Task<IActionResult> GetCompanies()
 		{
 			var companies = await _repository.Company.GetAllCompaniesAsync(trackChanges: false);
@@ -76,7 +76,7 @@ namespace CompanyEmployees.Controllers
 			return Ok(companiesToReturn);
 		}
 
-		[HttpPost]
+		[HttpPost(Name = "CreateCompany")]
 		[ServiceFilter(typeof(ValidationFilterAttribute))]
 		public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDTO company)
 		{
